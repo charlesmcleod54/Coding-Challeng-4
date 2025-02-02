d13ced29a9ef2ee20b10e25aaf96cdf5084639e7
Charles McLeod <charlesmcleod@usf.edu>
2025-02-02 15:36:58 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,12 @@ if (purchaseAmount > 100) {
 } else {
     console.log('Final Amount: $$ {purchaseAmount}');
 }
+// Task 2
+let sales = [120, 85, 200, 150, 90];
+let totalSales = 0;
+
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+
+console.log('Total sales: $${totalSales}');
