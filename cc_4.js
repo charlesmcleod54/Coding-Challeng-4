795e6ebb15cd203cfd20e6a6313e7057bd37123b
Charles McLeod <charlesmcleod@usf.edu>
2025-02-02 15:57:08 -0500

    Task 8 - Tax Calculation

--- cc_4.js
+++ cc_4.js
@@ -57,3 +57,14 @@ let orders = [101, 102, 103];
 orders.forEach(orderID => {
     console.log('Order ID: ${orderID}');
 });
+// Task 8
+function calculateTax(amount, taxRate) {
+    let tax = amount * taxRate;
+    return tax;
+}
+
+let amount = 115;
+let taxRate = 0.07;
+
+let tax = calculateTax(amount, taxRate);
+console.log('The calculated tax on $${amount} at a tax rate of ${taxRate * 100}% is: $${tax}');
